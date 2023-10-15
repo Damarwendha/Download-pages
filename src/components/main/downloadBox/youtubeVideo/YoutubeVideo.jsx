3f93e84eb3b48bb92cb8e640633dec0d56fa03d1
Sien Khumaen Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2023-10-15 14:33:46 +0700

    videoControls = 0

--- src/components/main/downloadBox/youtubeVideo/YoutubeVideo.jsx
+++ src/components/main/downloadBox/youtubeVideo/YoutubeVideo.jsx
@@ -4,7 +4,7 @@ import './youtubeVideo.css'
 export default function YoutubeVideo({videoId}) {
   return (
     <iframe
-      src={`https://www.youtube.com/embed/${videoId}`}
+      src={`https://www.youtube.com/embed/${videoId}&amp;controls=0`}
       title="YouTube video player"
       frameBorder="1"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
