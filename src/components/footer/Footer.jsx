196a2a6ad08d23b760a5522115c703d130f5ea1d
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-23 04:00:59 +0700

    add module.css and use current year in copyrigth

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,24 @@
 import Logo from "../shared/ui/logo/Logo";
 
-import "./footer.css";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-        <>
-          <div className="logo">
-            <Logo
-              img="images/youtube-logo-png-46031.png"
-              alt="youtube"
-              href="https://m.youtube.com/channel/UCyNj7Rfvk3N7thKppPnS3Jw"
-            />
-            <Logo
-              img="images/github.png"
-              alt="github"
-              href="https://github.com/Damarwendha"
-              width='35px'
-            />
-          </div>
-          <p>Copyright 2014 © Ledac Official</p>
-        </>
+    <div className={styles.footer}>
+      <div>
+        <Logo
+          img="images/youtube-logo-png-46031.png"
+          alt="youtube"
+          href="https://m.youtube.com/channel/UCyNj7Rfvk3N7thKppPnS3Jw"
+        />
+        <Logo
+          img="images/github.png"
+          alt="github"
+          href="https://github.com/Damarwendha"
+          width="35px"
+        />
+      </div>
+      <p>Copyright {new Date().getFullYear()} © Ledac Official</p>
     </div>
   );
 }
