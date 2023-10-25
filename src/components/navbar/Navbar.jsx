495c134849802b0daa2605b1d7167527b19e1c95
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-26 03:56:41 +0700

    making Navbar more reuseable

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,9 @@
 import styles from "./navbar.module.css";
-import { NavLink } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({children}) {
   return (
     <ul className={styles.navbar}>
-      <li>
-        <NavLink to="/">Config</NavLink>
-      </li>
-      <img src="images/channels4_profile.jpg" alt="logo" />
-      <li>
-        <NavLink to="/MlLite">ML Lite</NavLink>
-      </li>
+      {children}
     </ul>
   );
 }
