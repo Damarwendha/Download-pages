9b9fb1b4dcc3cbe268a5fb9f549ff3db431b040d
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-26 03:58:29 +0700

    setting up Navbar component

--- src/pages/mlLite/MlLite.jsx
+++ src/pages/mlLite/MlLite.jsx
@@ -1,10 +1,27 @@
+import { NavLink } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 
-export default function MlLite() {
+export default function MlLite({ type, id }) {
   return (
     <>
-      <Navbar />
+      <Navbar>
+        <li>
+          {type === "pw" ? (
+            <NavLink to="/">Config</NavLink>
+          ) : (
+            <NavLink to={`/Config/${id}`}>Config</NavLink>
+          )}
+        </li>
+        <img src="/images/channels4_profile.jpg" alt="logo" />
+        <li>
+          {type === "pw" ? (
+            <NavLink to="/MlLite">ML Lite</NavLink>
+          ) : (
+            <NavLink to={`/MlLite/${id}`}>ML Lite</NavLink>
+          )}
+        </li>
+      </Navbar>
       <h1>Coming Soon...</h1>
       <Footer />
     </>
