cb99705a2f1d2a92a9d9b27d668d34339fe78228
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-26 03:57:38 +0700

    setting up Navbar component

--- src/pages/configFps/ConfigFps.jsx
+++ src/pages/configFps/ConfigFps.jsx
@@ -1,12 +1,22 @@
+import { NavLink } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 import Main from "../../components/main/Main";
+import configFpsData from "./../../data/configFpsData.js";
 
 export default function MLBBConfig() {
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <li>
+          <NavLink to="/Config">Config</NavLink>
+        </li>
+        <img src="images/channels4_profile.jpg" alt="logo" />
+        <li>
+          <NavLink to="/MlLite">ML Lite</NavLink>
+        </li>
+      </Navbar>
+      <Main data={configFpsData} />
       <Footer />
     </>
   );
