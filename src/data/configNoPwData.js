c1bcd012606289e5ccd12bde348940f36123e6db
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-27 19:04:14 +0700

    add data for 4 version

--- src/data/configNoPwData.js
+++ src/data/configNoPwData.js
@@ -1,13 +1,48 @@
-const configNoPwData = {
-  patch: "NOLAN",
-  desc: [
-    {
-      versi: 4,
-      downloadLink: "https://sfile.mobi/1z9uc6f4326",
-      thumbnailId: "qNoL6L06LHE",
-      videoId: "qNoL6L06LHE?si=2gPaVjfQDystOIji",
-    },
-  ],
-};
+const configNoPwData = [
+  {
+    patch: "NOLAN",
+    desc: [
+      {
+        versi: 1,
+        downloadLink: "https://sfile.mobi/1z9uc6f4326",
+        thumbnailId: "qNoL6L06LHE",
+        videoId: "qNoL6L06LHE?si=2gPaVjfQDystOIji",
+      },
+    ],
+  },
+  {
+    patch: "NOLAN",
+    desc: [
+      {
+        versi: 2,
+        downloadLink: "https://sfile.mobi/1z9uc6f4326",
+        thumbnailId: "qNoL6L06LHE",
+        videoId: "qNoL6L06LHE?si=2gPaVjfQDystOIji",
+      },
+    ],
+  },
+  {
+    patch: "NOLAN",
+    desc: [
+      {
+        versi: 3,
+        downloadLink: "https://sfile.mobi/1z9uc6f4326",
+        thumbnailId: "qNoL6L06LHE",
+        videoId: "qNoL6L06LHE?si=2gPaVjfQDystOIji",
+      },
+    ],
+  },
+  {
+    patch: "NOLAN",
+    desc: [
+      {
+        versi: 4,
+        downloadLink: "https://sfile.mobi/1z9uc6f4326",
+        thumbnailId: "qNoL6L06LHE",
+        videoId: "qNoL6L06LHE?si=2gPaVjfQDystOIji",
+      },
+    ],
+  },
+];
 
 export default configNoPwData;
