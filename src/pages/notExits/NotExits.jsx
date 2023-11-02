673fdbd3ff9ef339092f8fe4944b7c074eb35c20
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-11-02 11:07:50 +0700

    add nabar to go home

--- src/pages/notExits/NotExits.jsx
+++ src/pages/notExits/NotExits.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+
 export default function NotExits() {
   return (
-    <h1>
-      This page isn&apos;t available. Sorry about that. Try searching for something
-      else.
-    </h1>
+    <>
+      <Navbar>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </Navbar>
+      <h1>
+        This page isn&apos;t available. Sorry about that. Try searching for
+        something else.
+      </h1>
+    </>
   );
 }
