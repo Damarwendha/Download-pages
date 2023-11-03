568dba9f5c5c31a90f4cbdb86620b9d98fdc14c6
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-11-03 11:36:17 +0700

    Moving to a new component LinkDownload

--- src/components/main/downloadBox/DownloadBox.jsx
+++ src/components/main/downloadBox/DownloadBox.jsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
-import { useCountdown } from "./../../../hooks/useCountdown.js";
 import Header from "./header/Header";
 import Thumbnail from "./thumbnail/Thumbnail";
-import Link from "../../shared/ui/link/Link";
-import Button from "../../shared/ui/button/Button";
 import Box from "../../shared/ui/box/Box";
 import styles from "./downloadBox.module.css";
+import LinkDownload from "./linkDownload/LinkDownload.jsx";
 
 export default function DownloadBox({ data, i }) {
-  const [showLink, setShowLink] = useState(false);
-  const { runCountdown, currentCount, isRunning } = useCountdown(2, () =>
-    setShowLink(true)
-  );
-
-  function handleButton() {
-    runCountdown();
-  }
-
   return (
     <Box
       margin="0px 0px 10px 0px"
@@ -27,24 +15,7 @@ export default function DownloadBox({ data, i }) {
       className={styles.boxmain}
     >
       <Header v={data.versi} i={i} />
-
-      {showLink ? (
-        <Link url={`${data.downloadLink}`} />
-      ) : (
-        <Button
-          text={
-            isRunning ? `Tunggu sebentar... ${currentCount}` : "Download disini"
-          }
-          onClick={handleButton}
-          padding="5px 20px"
-          margin="5px"
-          backgroundColor={isRunning ? "black" : "blue"}
-          isDisabled={isRunning}
-          border={isRunning ? "0px" : null}
-          className={styles.button}
-        />
-      )}
-
+      <LinkDownload url={data.downloadLink} />
       <Thumbnail thumbnailId={data.thumbnailId} videoId={data.videoId} />
     </Box>
   );
